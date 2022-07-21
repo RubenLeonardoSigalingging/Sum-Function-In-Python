db84452e6e64da0fb42319305217d91d8f2ea50e
Ruben Leonardo Sigalingging <106874389+RubenLeonardoSigalingging@users.noreply.github.com>
2022-07-21 23:23:25 +0700

    Add files via upload

---
+++ FungsiSum.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+
+# Fungsi sum()
+# APA ITU FUNGSI SUM?
+# Fungsi sum() berfungsi untuk menjumlahkan semua bilangan atau item yang menjadi argumennya.
+
+
+# Sintaks:
+# sum(iterable[,start])
+
+
+from os import system
+system("clear")
+
+
+def Fungsi_Sum_Pada_Python3(Data,Created_By="Ruben Leonardo Sigalingging"):
+	return Data
+Data=[5,23,2]
+print(f"Hasil Dari Fungsi Sum: {sum(Data)}")
+print(f"Hasil Dari Fungsi Sum + 3: {sum(Data,3)}")
